a3196e7e5b639376c842c2f20e463e1bd3ad66f7
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-02-12 14:33:10 +0200

    Add interface Const.

---
+++ src/main/java/ua/block02/task02/game/Const.java
@@ -0,0 +1,14 @@
+package ua.block02.task02.game;
+
+/**
+ * Created on 12.02.2019.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+public interface Const {
+
+    public static final int MIN_VALUE = 0;
+    public static final int MAX_VALUE = 100;
+}
