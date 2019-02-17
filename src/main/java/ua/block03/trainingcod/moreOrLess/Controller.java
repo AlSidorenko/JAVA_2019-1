9ddebcdc02c9bf2c86b6b08b844ec8248915ec01
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-02-17 21:45:16 +0200

    Block_03, Training code - game More or Less.

---
+++ src/main/java/ua/block03/trainingcod/moreOrLess/Controller.java
@@ -0,0 +1,62 @@
+package ua.block03.trainingcod.moreOrLess;
+
+import java.util.Scanner;
+
+/**
+ * Created by Student on 22.02.2017.
+ */
+public class Controller implements TextConstants {
+
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    // The Work method
+    public void processUser(){
+        Scanner sc = new Scanner(System.in);
+
+        model.setPrimaryBarrier(GlobalConstants.PRIMARY_MIN_BARRIER,
+                                GlobalConstants.PRIMARY_MAX_BARRIER);
+
+        model.setSecretValue();
+        System.out.println(model.getSecretValue());
+
+        while (model.checkValue(inputIntValueWithScanner(sc)));
+
+        view.printMessage(CONGRATULATION + model.getSecretValue());
+        view.printMessage(YOUR_WAY + String.valueOf(model.getYourWay()));
+    }
+
+    private int inputIntValueWithScanner(Scanner sc) {
+        int res = 0;
+        view.printMessage(getInputIntMessage());
+        while (true) {
+            // check int-value
+            while (!sc.hasNextInt()) {
+                view.printMessage(WRONG_INPUT_INT_DATA + getInputIntMessage());
+                sc.next();
+            }
+            // check value into diapason
+            if ((res = sc.nextInt()) <= model.getMinBarrier() ||
+                    res >= model.getMaxBarrier()) {
+                view.printMessage(WRONG_INPUT_INT_DATA + getInputIntMessage());
+                continue;
+            }
+            break;
+        }
+        return res;
+    }
+
+    private String getInputIntMessage() {
+        return view.concatenationString(
+                INPUT_INT_DATA, OPENS_SQUARE_BRACKET,
+                String.valueOf(model.getMinBarrier()), SPACE_SING,
+                String.valueOf(model.getMaxBarrier()),
+                CLOSING_SQUARE_BRACKET, SPACE_SING,
+                EQUAL_SING, SPACE_SING );
+    }
+}
